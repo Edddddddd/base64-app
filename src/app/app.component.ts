662c127d1c81a6253126ca358cba06451e038336
Eduardo Silva <eduardo.cristobal.silva@gmail.com>
2020-12-16 11:07:37 -0300

    modificaciones de estilos y script pkg json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'base64-app';
+  inputBase64 = '';
+  outputText = '';
+  rows = 10;
+  convertirToBase64(): void {
+    this.outputText = '';
+    this.outputText = btoa(this.inputBase64)
+  }
+
+  convertirToText(): void {
+    this.outputText = '';
+    this.outputText = atob(this.inputBase64)
+  }
+
+  clear(): void {
+    this.outputText = '';
+    this.inputBase64 = '';
+    this.rows = 10;
+  }
+
+
+
+  descargarFile(): void {
+    // The Base64 string of a simple PDF file
+      var b64 = this.inputBase64;
+
+      var link = document.createElement('a');
+      link.innerHTML = 'Download PDF file';
+      link.download = 'file.bin';
+      link.href = 'data:application/octet-stream;base64,' + b64;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {DemoMaterialModule} from './material-module';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import {DemoMaterialModule} from './material-module';
   imports: [
     BrowserModule,
     DemoMaterialModule,
+    FormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
